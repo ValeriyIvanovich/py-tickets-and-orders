745307f2388b890dffb755228931f0a85e44b093
Valeriy Semenik <valeriy.semenik@gmail.com>
2025-11-03 17:52:19 +0200

    Solution 03112025 1

--- db/models.py
+++ db/models.py
@@ -76,7 +76,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return str(f"<Order: {self.created_at:%Y-%m-%d %H:%M:%S}>")
+        return str(f"{self.created_at:%Y-%m-%d %H:%M:%S}")
 
 
 class Ticket(models.Model):
@@ -101,33 +101,32 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
         return (
-            f"<Ticket: {self.movie_session.movie.title} "
+            f"{self.movie_session.movie.title} "
             f"{self.movie_session.show_time} "
-            f"(row: {self.row}, seat: {self.seat})>"
+            f"(row: {self.row}, seat: {self.seat})"
         )
 
     def clean(self) -> None:
-        if self.row < 1 or self.row > self.movie_session.cinema_hall.rows:
+        hall = self.movie_session.cinema_hall
+
+        if not (1 <= self.row <= hall.rows):
             raise ValidationError(
                 {
                     "row": [
-                        f"row number must be in available range: "
-                        f"(1, rows): ("
-                        f"1, "
-                        f"{self.movie_session.cinema_hall.rows})"
+                        f"row number must be "
+                        f"in available range: (1, rows): "
+                        f"(1, {hall.rows})"
                     ]
                 }
             )
-        if (
-                self.seat < 1
-                or self.seat > self.movie_session.cinema_hall.seats_in_row
-        ):
+
+        if not (1 <= self.seat <= hall.seats_in_row):
             raise ValidationError(
                 {
                     "seat": [
-                        f"seat number must be in available range: "
-                        f"(1, seats_in_row): (1, "
-                        f"{self.movie_session.cinema_hall.seats_in_row})"
+                        f"seat number must be in "
+                        f"available range: (1, seats_in_row): "
+                        f"(1, {hall.seats_in_row})"
                     ]
                 }
             )
